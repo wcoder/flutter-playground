aab0a4432a61d825ad05b362438c1e3e18cb5af9
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-02 18:56:18 +0300

    Add empty list view

--- expense_app/lib/widgets/transaction_list.dart
+++ expense_app/lib/widgets/transaction_list.dart
@@ -11,54 +11,73 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) {
-          final transaction = transactions[index];
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2
-                      )
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${transaction.amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      transaction.title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(transaction.date),
-                      style: TextStyle(
-                          color: Colors.grey
-                      ),
-                    )
-                  ],
-                )
-              ],
+      child: transactions.isEmpty
+        ? Column(
+          children: <Widget>[
+            Text(
+              "No transactions added yet!",
+              style: Theme.of(context).textTheme.title
             ),
-          );
-        },
-      ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 200,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        )
+        : ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (context, index) {
+            final transaction = transactions[index];
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2
+                        )
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transaction.amount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transaction.title,
+                        style: Theme.of(context).textTheme.title,
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(transaction.date),
+                        style: TextStyle(
+                            color: Colors.grey
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+        ),
     );
   }
 }
