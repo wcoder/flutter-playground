84feadc93517f5921aac4d90332e1b819710c7b0
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-04 00:18:16 +0300

    Add meals

--- meals_app/lib/pages/category_meals_page.dart
+++ meals_app/lib/pages/category_meals_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/dummy_data.dart';
 
 class CategoryMealsPage extends StatelessWidget {
   @override
@@ -6,15 +7,19 @@ class CategoryMealsPage extends StatelessWidget {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Recipes For the Category!'
-        ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
