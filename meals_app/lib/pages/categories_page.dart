03c4354a1318f566944094eff00aa9a8c3c4be73
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-03 23:07:15 +0300

    Add app classes

---
+++ meals_app/lib/pages/categories_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../services/dummy_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemCount: DUMMY_CATEGORIES.length,
+        itemBuilder: (context, index) {
+          final category = DUMMY_CATEGORIES[index];
+          return CategoryItem(category.title, category.color);
+        },
+      ),
+    );
+  }
+}
