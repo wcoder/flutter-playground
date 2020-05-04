bd16db10c71f80ec9455bfb1bdff1cb928992e4e
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-04 15:17:38 +0300

    Add products list

---
+++ shop_app/lib/widgets/product_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
