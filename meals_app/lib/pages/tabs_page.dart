94092afbd263dd0cdb6ea3fffcdecd0eae43b62f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-04 11:47:12 +0300

    Change to bottom bar

--- meals_app/lib/pages/tabs_page.dart
+++ meals_app/lib/pages/tabs_page.dart
@@ -8,31 +8,52 @@ class TabsPage extends StatefulWidget {
 }
 
 class _TabsPageState extends State<TabsPage> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesPage(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesPage(),
+      'title': 'Favorites',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
-          ),
+          title: Text(_pages[_selectedPageIndex]['title']),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesPage(),
-            FavoritesPage(),
-          ],
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          currentIndex: _selectedPageIndex,
+          selectedItemColor: Theme.of(context).accentColor,
+          unselectedItemColor: Colors.white,
+          backgroundColor: Theme.of(context).primaryColor,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text(_pages[0]['title']),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text(_pages[1]['title']),
+              backgroundColor: Theme.of(context).primaryColor,
+            )
+          ]
         ),
       ),
     );
