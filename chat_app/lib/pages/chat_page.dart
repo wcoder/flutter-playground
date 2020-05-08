eb17eba6afb0af229332d3a441582a2e4959878a
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-08 12:43:16 +0300

    Setup firestore

--- chat_app/lib/pages/chat_page.dart
+++ chat_app/lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -10,6 +11,17 @@ class ChatPage extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text("This works!"),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+            .collection("chats/mivpJiPT4e0Voy3GTL6u/messages")
+            .snapshots()
+            .listen((snapshot) {
+              print(snapshot);
+            });
+        },
       ),
     );
   }
