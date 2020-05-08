4a5fb5d747997d6e4071134ef6b631b3b9388879
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-08 12:53:43 +0300

    Basic show/add messages

--- chat_app/lib/pages/chat_page.dart
+++ chat_app/lib/pages/chat_page.dart
@@ -2,25 +2,44 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
+  final chatCollectionRef = Firestore.instance
+      .collection("chats/mivpJiPT4e0Voy3GTL6u/messages");
+
   @override
   Widget build(BuildContext context) {
+    final messagesStream = chatCollectionRef.snapshots();
+
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text("This works!"),
-        ),
+      body: StreamBuilder(
+        stream: messagesStream,
+        builder: (context, streamSnapshot) {
+          switch (streamSnapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.active:
+              final messages = streamSnapshot.data.documents;
+              return ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) => Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text(messages[index]['text']),
+                ),
+              );
+            default:
+              return Center(
+                child: Text('Nothing!'),
+              );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          Firestore.instance
-            .collection("chats/mivpJiPT4e0Voy3GTL6u/messages")
-            .snapshots()
-            .listen((snapshot) {
-              print(snapshot);
-            });
+          chatCollectionRef.add({
+            "text": "This was added by clicking the button!",
+          });
         },
       ),
     );
