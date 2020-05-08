7dd9c7616672977670e0710463169a03ed0be40d
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-08 20:42:51 +0300

    Add firebase messaging / push notifications, ios need certs

--- chat_app/lib/pages/chat_page.dart
+++ chat_app/lib/pages/chat_page.dart
@@ -1,9 +1,39 @@
 import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:chat_app/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(
+      onMessage: (msg) {
+        print(msg);
+        return;
+      },
+      onLaunch: (msg) {
+        print(msg);
+        return;
+      },
+      onResume: (msg) {
+        print(msg);
+        return;
+      },
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
