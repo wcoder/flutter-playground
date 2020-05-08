e8bb5594bc4efde435babe4a8ee9cf4e3ce4f11a
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-05-08 13:07:23 +0300

    Add styles

--- chat_app/lib/widgets/auth/auth_form.dart
+++ chat_app/lib/widgets/auth/auth_form.dart
@@ -43,6 +43,7 @@ class _AuthFormState extends State<AuthForm> {
                     onPressed: () {},
                   ),
                   FlatButton(
+                    textColor: Theme.of(context).primaryColor,
                     child: Text("Create new account"),
                     onPressed: () {},
                   ),
