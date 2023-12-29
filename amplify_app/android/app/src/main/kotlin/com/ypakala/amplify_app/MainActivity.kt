c5e9dc712d8aedfa6731820e024cce1e7be5e499
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2023-12-29 20:39:48 +0100

    Create empty app

---
+++ amplify_app/android/app/src/main/kotlin/com/ypakala/amplify_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ypakala.amplify_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ amplify_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:amplify_app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
